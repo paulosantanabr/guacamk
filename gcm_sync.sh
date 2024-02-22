0921afd44e827bf3a18635eb2be18fa20b736bf5
pauloadrianodotcom <121560100+pauloadrianodotcom@users.noreply.github.com>
2024-02-21 21:02:17 -0300

    Create gcm_sync.sh

---
+++ gcm_sync.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#1 - Autenticacao
+
+#Apache Guacamole
+#Username
+#Password
+
+#Checkmk
+#Username
+#Password
+
+export TOKEN=$(curl -s -k -X POST  https://localhost/api/tokens -d 'username=guacadmin&password=pa55wrd' | jq -r .authToken)
+
+echo ====================================================
+echo 1 - Retrieving connection ids from Apache Guacamole
+curl -s -k -X GET -H 'Content-Type: application/json' https://localhost/api/session/data/postgresql/connections?token=$TOKEN | jq | grep -o '"identifier":\s*"[0-9]\+"' | tr -d '"identifier": ' > ./files/gcm_ids.file
+echo ====================================================
+echo 2 - Retrieving connection details from Apache Guacamole
+filename="./files/gcm_ids.file"
+while IFS= read -r CONNECTIONID;
+do
+CHECKCONNECTIONID=$(curl -s -k -X GET -H 'Content-Type: application/json' https://localhost/api/session/data/postgresql/connections/$CONNECTIONID?token=$TOKEN | jq .name | tr -d '"' )
+CHECKCONNECTIONPROTOCOL=$(curl -s -k -X GET -H 'Content-Type: application/json' https://localhost/api/session/data/postgresql/connection s/$CONNECTIONID?token=$TOKEN | jq .protocol | tr -d '"')
+CHECKCONNECTIONIP=$(curl -s -k -X GET -H 'Content-Type: application/json' https://localhost/api/session/data/postgresql/connections/$CONNECTIONID/parameters?token=$TOKEN | jq .hostname | tr -d '"')
+echo $CONNECTIONID,$CHECKCONNECTIONID,$CHECKCONNECTIONIP,$CHECKCONNECTIONPROTOCOL >> ./files/gcm_connections.file
+done < "$filename"
+echo ====================================================
+echo 3 - Retrieving current hosts from Checkmk
+./cmk_gethosts.sh | grep id > ./files/cmk_hosts.file
+echo ====================================================
+echo 4 - Create connections on Checkmk
+
+filename="./files/gcm_connections.file"
+
+while IFS=',' read -r connectionid name ip protocol; do
+if grep -q "$name" ./files/cmk_hosts.file
+then
+echo Already on Checkmk - Connection ID: $connectionid Name: $name $ip $protocol
+else
+echo NOT on Checkmk - Connection ID: $connectionid Name: $name $ip $protocol    
+./cmk_createhosts.sh $connectionid $name $ip $protocol > ./logs/mklog_$name.log
+fi
+done < "$filename"
+echo ===================================================
+echo 5 - Activate changes on Checkmk
+./cmk_activatechanges.sh
+echo ====================================================
+echo 6 -Cleanup
+echo Removing gcm_ids.file
+rm ./files/gcm_ids.file
+echo Removing gcm_connections.file
+rm ./files/gcm_connections.file
+echo Removing cmk_hosts.file
+rm ./files/cmk_hosts.file
+echo ====================================================
+exit
