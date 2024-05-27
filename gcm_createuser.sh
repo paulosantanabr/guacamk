947896d16648358009b8a4809d036ef60098cfd4
pauloadrianodotcom <121560100+pauloadrianodotcom@users.noreply.github.com>
2024-05-27 20:52:29 -0300

    Create gcm_createuser.sh

---
+++ gcm_createuser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+GCMSERVER=
+GCMUSR=
+GCMPWD=
+
+gcm_usr=
+gcm_pwd=
+
+export TOKEN=$(curl -s -k -X POST https://$GCMSERVER/api/tokens -d 'username='$GCMUSR'&password='$GCMPWD'' | jq -r .authToken)
+
+curl -s -k -X POST -H 'Content-Type: application/json' https://$GCMSERVER/api/session/data/postgresql/users?token=$TOKEN --data-binary '{"username":"'"$gcm_usr"'","password":"'"$gcm_pwd"'","attributes":{}}'
