3501785abfcc62bd1fc7c48607f931d3b7c84fd2
pauloadrianodotcom <121560100+pauloadrianodotcom@users.noreply.github.com>
2024-06-18 19:01:49 -0300

    Update gcm_createuser.sh

--- gcm_createuser.sh
+++ gcm_createuser.sh
@@ -6,6 +6,11 @@ GCMPWD=
 gcm_usr=
 gcm_pwd=
 
+passwordgeneration() {
+gcm_pwd=$(echo Gcm!$1123@789)
+echo $gcm_pwd
+}
+
 export TOKEN=$(curl -s -k -X POST https://$GCMSERVER/api/tokens -d 'username='$GCMUSR'&password='$GCMPWD'' | jq -r .authToken)
 
 curl -s -k -X POST -H 'Content-Type: application/json' https://$GCMSERVER/api/session/data/postgresql/users?token=$TOKEN --data-binary '{"username":"'"$gcm_usr"'","password":"'"$gcm_pwd"'","attributes":{}}'
