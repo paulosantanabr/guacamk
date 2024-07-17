f54d4f62abb1c3d485d679956cea3a0a1ffbc164
Paulo Santana <121560100+paulosantanabr@users.noreply.github.com>
2024-07-16 21:49:25 -0300

    Update gcm_delete.sh

--- gcm_delete.sh
+++ gcm_delete.sh
@@ -12,7 +12,7 @@ CONNECTIONID=$NOTIFY_HOSTLABEL_gcm_id
 #echo $GCMSERVER $GCMUSR GCMPWD $CONNECTIONID
 
 #1 - Authentication
-export TOKEN=$(curl -s -k -X POST  https://$GCMSERVER/api/tokens -d 'username='$GCMUSR'&password='$GCMPWD'' | jq -r .authToken)
+cmk_delete=$(curl -s -k -X DELETE https://$GCMSERVER/api/session/data/postgresql/connections/$CONNECTIONID?token=$TOKEN)
 
-curl -s -k -X DELETE https://$GCMSERVER/api/session/data/postgresql/connections/$CONNECTIONID?token=$TOKEN
-
+echo Hostname: $NOTIFY_HOSTNAME / gcm_id: $NOTIFY_HOSTLABEL_gcm_id / gcm_parentid: $NOTIFY_HOSTLABEL_gcm_parentid / Action: DELETE / Integration: Apache Guacamole / Status: $cmk_delete >> 
+echo Hostname: $NOTIFY_HOSTNAME / gcm_id: $NOTIFY_HOSTLABEL_gcm_id / gcm_parentid: $NOTIFY_HOSTLABEL_gcm_parentid / Action: DELETE / Integration: Apache Guacamole / Status: $cmk_delete
