7c9eb256efd58017e89a1a8de2b7233fada9a0df
Paulo Santana <121560100+paulosantanabr@users.noreply.github.com>
2024-07-16 22:01:21 -0300

    Update gcm_delete.sh

--- gcm_delete.sh
+++ gcm_delete.sh
@@ -14,5 +14,5 @@ CONNECTIONID=$NOTIFY_HOSTLABEL_gcm_id
 #1 - Authentication
 cmk_delete=$(curl -s -k -X DELETE https://$GCMSERVER/api/session/data/postgresql/connections/$CONNECTIONID?token=$TOKEN)
 
-echo Hostname: $NOTIFY_HOSTNAME / gcm_id: $NOTIFY_HOSTLABEL_gcm_id / gcm_parentid: $NOTIFY_HOSTLABEL_gcm_parentid / Action: DELETE / Integration: Apache Guacamole / Status: $cmk_delete >> 
+echo Hostname: $NOTIFY_HOSTNAME / gcm_id: $NOTIFY_HOSTLABEL_gcm_id / gcm_parentid: $NOTIFY_HOSTLABEL_gcm_parentid / Action: DELETE / Integration: Apache Guacamole / Status: $cmk_delete >> /var/log/guacamk-deleteconnection.log
 echo Hostname: $NOTIFY_HOSTNAME / gcm_id: $NOTIFY_HOSTLABEL_gcm_id / gcm_parentid: $NOTIFY_HOSTLABEL_gcm_parentid / Action: DELETE / Integration: Apache Guacamole / Status: $cmk_delete
